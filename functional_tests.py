c434aa8a2fdec57362cc1a654f4bd22c698878db
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-09-21 16:18:06 -0300

    Rewritten to use the unittest module.

--- functional_tests.py
+++ functional_tests.py
@@ -2,12 +2,27 @@
 Name:         Roger Silva Santos Aguiar
 Function:     Learning TDD
 Initial date: September 17, 2020
-Last update:  September 17, 2020
+Last update:  September 21, 2020
  """
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('Http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ =='__main__':
+    unittest.main(warnings='ignore')
